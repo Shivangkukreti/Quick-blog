951340b970f9a4481f452553db5a443054c683bf
Shivangkukreti <shivangkukretiwork@gmail.com>
2025-08-16 19:55:02 +0530

    aa

--- frontend/src/components/hambur.jsx
+++ frontend/src/components/hambur.jsx
@@ -24,7 +24,7 @@ function Hambur() {
   },[])
   return (
     <div className="absolute sm:hidden  top-0 left-0  bottom-0 right-0 z-20 backdrop-blur-sm flex justify-end items-center">
-      <div className="bg-white relative shadow-2xl w-30 h-[100vh] py-10">
+      <div className="bg-white relative shadow-2xl w-50 h-[100vh] py-10">
         <div
           onClick={() => setham(false)}
           className="absolute top-0 font-extrabold text-gray-700 m-2"
@@ -32,12 +32,12 @@ function Hambur() {
           X
         </div>
 
-        <div
+        <div  
           onClick={() => {
             setham(false);
             navi("/");  
           }}
-          className="text-center bg-gradient-to-l from-gray-500 to-white duration-300 hover:text-white transition-colors hover:from-white hover:to-gray-500  cursor-pointer font-medium text-gray-700 mt-5 border-y-1 border-gray-400"
+          className="text-center cursor-pointer bg-indigo-600 font-medium text-white  mt-5 border-y-1 border-gray-400"
         >
           home
         </div>
@@ -48,7 +48,7 @@ function Hambur() {
                 setham(false);
                 navi("/admin");
               }}
-              className="text-center bg-gradient-to-l from-gray-500 to-white duration-300 transition-colors hover:from-white hover:to-gray-500  cursor-pointer font-medium text-gray-700 mt-5 border-y-1 border-gray-400"
+              className="text-center bg-indigo-600  cursor-pointer font-medium text-white mt-5 border-y-1 border-gray-400"
             >
               Dashboard
             </div>
@@ -57,7 +57,7 @@ function Hambur() {
                 setham(false);
                 logout();
               }}
-              className="text-center  bg-gradient-to-l from-gray-500 to-white duration-300 transition-colors hover:from-white hover:to-gray-500  cursor-pointer font-medium text-gray-700 mt-5 border-y-1 border-gray-400"
+              className="text-center  text-white  cursor-pointer font-medium bg-indigo-600 mt-5 border-y-1 border-gray-400"
             >
               Logout
             </div>
@@ -68,7 +68,7 @@ function Hambur() {
               setham(false);
               setlogin(true);
             }}
-            className="text-center bg-gradient-to-l from-gray-500 to-white duration-300 transition-colors hover:from-white hover:to-gray-500 cursor-pointer font-medium text-gray-700 mt-5 border-y-1 border-gray-400"
+            className="text-center bg-indigo-600 cursor-pointer font-medium text-white mt-5 border-y-1 border-gray-400"
           >
             login
           </div>
